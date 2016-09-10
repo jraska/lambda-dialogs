ee03346ee437647b23b82f7acddb80b8d7c87585
Josef Raska <josef.raska@gmail.com>
2016-09-10 15:29:26 +0100

    Add simple test for builder dialog buttons

---
+++ sample/src/test/java/com.jraska.dialog/sample/LambdaDialogsBuilderTest.java
@@ -0,0 +1,42 @@
+package com.jraska.dialog.sample;
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AlertDialog;
+import com.jraska.dialog.LambdaDialogs;
+import org.assertj.android.api.Assertions;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowDialog;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 23)
+public class LambdaDialogsBuilderTest {
+  FragmentActivity activity;
+
+  @Before
+  public void setUp() {
+    activity = Robolectric.setupActivity(FragmentActivity.class);
+  }
+
+  @Test
+  public void whenShown_thenButtonsTextsCorrect() {
+    String positive = "126kas";
+    String neutral = "12asi";
+    String negative = "98io8";
+
+    LambdaDialogs.builder(activity)
+        .positiveText(positive)
+        .neutralText(neutral)
+        .negativeText(negative)
+        .show();
+
+    AlertDialog dialog = (AlertDialog) ShadowDialog.getLatestDialog();
+    Assertions.assertThat(dialog.getButton(AlertDialog.BUTTON_POSITIVE)).hasText(positive);
+    Assertions.assertThat(dialog.getButton(AlertDialog.BUTTON_NEUTRAL)).hasText(neutral);
+    Assertions.assertThat(dialog.getButton(AlertDialog.BUTTON_NEGATIVE)).hasText(negative);
+  }
+}
