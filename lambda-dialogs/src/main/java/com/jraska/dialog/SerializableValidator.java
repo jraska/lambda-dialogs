132214430b2fe0f0b152ede3f0d93b288f96f45b
Josef Raska <josef.raska@gmail.com>
2017-03-29 06:34:02 +0100

    Remove Lombok. Resolves #12

--- lambda-dialogs/src/main/java/com/jraska/dialog/SerializableValidator.java
+++ lambda-dialogs/src/main/java/com/jraska/dialog/SerializableValidator.java
@@ -1,15 +1,25 @@
 package com.jraska.dialog;
 
-import lombok.SneakyThrows;
 
 import java.io.*;
 
 final class SerializableValidator {
 
-  @SneakyThrows @SuppressWarnings("unchecked")
+  private static final Serializable NULL_IS_SERIALIZABLE = null;
+
   <T extends Serializable> T validateSerializable(T serializable) {
+    try {
+      return validateUnchecked(serializable);
+    } catch (IOException | ClassNotFoundException e) {
+      throw new IllegalArgumentException(serializable.getClass()
+          + " is not Serializable", e);
+    }
+  }
+
+  @SuppressWarnings("unchecked")
+  private <T extends Serializable> T validateUnchecked(T serializable) throws IOException, ClassNotFoundException {
     if (serializable == null) {
-      return null;
+      return (T) NULL_IS_SERIALIZABLE;
     }
 
     ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
--- lambda-dialogs/src/test/java/com/jraska/dialog/LambdaDialogsValidateEagerlyTest.java
+++ lambda-dialogs/src/test/java/com/jraska/dialog/LambdaDialogsValidateEagerlyTest.java
@@ -6,8 +6,6 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.NotSerializableException;
-
 import static org.mockito.Mockito.mock;
 
 public class LambdaDialogsValidateEagerlyTest {
@@ -24,53 +22,7 @@ public class LambdaDialogsValidateEagerlyTest {
     LambdaDialogs.validateEagerly(false); // put it back to default state after each test
   }
 
-  @Test(expected = NotSerializableException.class)
-  public void givenStaticConfig_whenBuildPositiveMethod_thenCrashesOnSerialization() {
-    LambdaDialogs.validateEagerly(true);
-
-    LambdaDialogs.builder(mock(FragmentActivity.class))
-        .positiveMethod(notSerializable)
-        .build();
-  }
-
-  @Test(expected = NotSerializableException.class)
-  public void givenStaticConfig_whenBuildNeutralMethod_thenCrashesOnSerialization() {
-    LambdaDialogs.validateEagerly(true);
-
-    LambdaDialogs.builder(mock(FragmentActivity.class))
-        .neutralMethod(notSerializable)
-        .build();
-  }
-
-  @Test(expected = NotSerializableException.class)
-  public void givenStaticConfig_whenBuildNegativeMethod_thenCrashesOnSerialization() {
-    LambdaDialogs.validateEagerly(true);
-
-    LambdaDialogs.builder(mock(FragmentActivity.class))
-        .negativeMethod(notSerializable)
-        .build();
-  }
-
-  @Test
-  public void givenStaticConfig_whenBuilderEagerValueOverwritten_thenPasses() {
-    LambdaDialogs.validateEagerly(true);
-
-    LambdaDialogs.builder(mock(FragmentActivity.class))
-        .negativeMethod(notSerializable)
-        .validateEagerly(false)
-        .build();
-  }
-
-  @Test(expected = NotSerializableException.class)
-  public void givenStaticConfig_whenBuildDialogFactory_thenCrashesOnSerialization() {
-    LambdaDialogs.validateEagerly(true);
-
-    LambdaDialogs.builder(mock(FragmentActivity.class))
-        .dialogFactory(notSerializable)
-        .build();
-  }
-
-  @Test(expected = NotSerializableException.class)
+  @Test(expected = IllegalArgumentException.class)
   public void givenStaticConfig_whenDelegateMethod_thenCrashesOnSerialization() {
     LambdaDialogs.validateEagerly(true);
 
@@ -79,7 +31,7 @@ public class LambdaDialogsValidateEagerlyTest {
         .build();
   }
 
-  @Test(expected = NotSerializableException.class)
+  @Test(expected = IllegalArgumentException.class)
   public void givenStaticConfig_whenDelegateParametrizedMethod_thenCrashesOnSerialization() {
     LambdaDialogs.validateEagerly(true);
 
@@ -101,7 +53,6 @@ public class LambdaDialogsValidateEagerlyTest {
   }
 
   static class NotSerializable implements ActivityMethod<FragmentActivity>,
-      ActivityDialogMethodParam<FragmentActivity, DialogFields>,
       ActivityDialogMethod<FragmentActivity> {
     @SuppressWarnings("unused")
     private final Object notSerializable = new Object();
@@ -110,17 +61,13 @@ public class LambdaDialogsValidateEagerlyTest {
       throw new UnsupportedOperationException("Not implemented!");
     }
 
-    @Override public Dialog onCreateDialog(FragmentActivity activity, DialogFields parameter) {
-      throw new UnsupportedOperationException("Not implemented!");
-    }
-
     @Override public Dialog onCreateDialog(FragmentActivity activity) {
       throw new UnsupportedOperationException("Not implemented!");
     }
   }
 
-  static class NotSerializableStringParam implements
-      ActivityDialogMethodParam<FragmentActivity, String> {
+  static class NotSerializableStringParam
+      implements ActivityDialogMethodParam<FragmentActivity, String> {
     @SuppressWarnings("unused")
     private final Object notSerializable = new Object();
 
